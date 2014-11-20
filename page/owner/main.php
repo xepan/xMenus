a3c209288992af5d64c39e7ff12226bb8a4f88a3
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2014-11-20 14:55:27 +0530

    Icon added

--- page/owner/main.php
+++ page/owner/main.php
@@ -2,7 +2,9 @@
 
 class page_xMenus_page_owner_main extends page_componentBase_page_owner_main {
 	function initMainPage(){
-				$this->add('H3')->setHTML('Component Owner Main Page<small> no option At backend</small>');
+
+		$this->h1->setHTML('<i class="fa fa-navicon"></i> '.$this->component_name. '<small>Different kinds of Responsive and Custom Menus</small>');
+		$this->add('H3')->setHTML('<small> no option At backend</small>');
 
 	}
 
